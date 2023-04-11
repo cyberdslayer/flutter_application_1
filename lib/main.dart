0e51ab28b7d1b71f9ac14684ac72a0ac8cd4aa16
cyberdslayer <95081360+cyberdslayer@users.noreply.github.com>
2023-04-11 22:00:04 +0530

    day 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,11 +11,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    int days = 30;
-    String name = " Codeup"
+    bringVegetables();
     return MaterialApp(
       home: HomePage(),
+      // theme: ThemeData()
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch:  Colors.deepPurple,
       ),
+
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+
+      routes: {
+        "/" : (context) => LoginPage(),
+        "/login":(context) => LoginPage(),
+      },
     );
   }
+}
+
+
+bringVegetables({@required bool thaila= true, int rupees = 100, }){
+  
 }
